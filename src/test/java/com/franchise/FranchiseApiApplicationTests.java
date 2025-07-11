071c439e7cadfd7bcb33dda2612f3c3e0d63c0cf
Wilder Rincon <wildsrincon.developer@gmail.com>
2025-07-11 17:22:33 -0400

    feat: Initial commit and config implementation

---
+++ src/test/java/com/franchise/FranchiseApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.franchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FranchiseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Esta prueba verifica que el contexto de Spring Boot se carga correctamente
+    }
+}
