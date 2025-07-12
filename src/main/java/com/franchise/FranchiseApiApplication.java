85b52cf3c97ba327930c1f740ea58084ce14047c
Wilder Rincon <wildsrincon.developer@gmail.com>
2025-07-12 12:50:12 -0400

    feat: Adding initial configuration spring boot and mongodb

--- src/main/java/com/franchise/FranchiseApiApplication.java
+++ src/main/java/com/franchise/FranchiseApiApplication.java
@@ -2,10 +2,28 @@ package com.franchise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class FranchiseApiApplication {
+
     public static void main(String[] args) {
+        System.out.println("🚀 Iniciando Franchise API...");
         SpringApplication.run(FranchiseApiApplication.class, args);
+    }
+}
+
+@RestController
+class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "¡Franchise API funcionando!";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Test exitoso - " + new java.util.Date();
     }
 }
